a0795695489f3a054184b692d40489aaa6fdb86b
Anand Sakthivel <anand@Anithas-MacBook-Air.local>
2024-06-08 14:12:31 -0500

    dijiktras two stack

--- src/main/kotlin/com/me/learn/personal/ds/stack/dtsa/DijikstrasTwoStackAlgorithm.kt
+++ src/main/kotlin/com/me/learn/personal/ds/stack/dtsa/DijikstrasTwoStackAlgorithm.kt
@@ -1,4 +1,41 @@
 package com.me.learn.personal.ds.stack.dtsa
 
-class DijikstrasTwoStackAlgorithm {
+import com.me.learn.personal.ds.stack.Stack
+
+class DijikstrasTwoStackAlgorithm(
+    val expression: String
+) {
+    fun execute() {
+        val valueStack = Stack<Int>(expression.length)
+        val operatorStack = Stack<Char>(expression.length)
+        expression.forEach {
+            if (it == '+' || it == '*' || it == '-' || it == '/' || it == '%') {
+                operatorStack.push(it)
+            } else if (it == ')') {
+                val topElement = valueStack.pop() ?: 0
+                val previousElement = valueStack.pop() ?: 0
+                val operator = operatorStack.pop()
+
+                val result = when (operator) {
+                    '+' -> previousElement + topElement
+                    '-' -> previousElement - topElement
+                    '*' -> previousElement * topElement
+                    '/' -> previousElement / topElement
+                    '%' -> previousElement % topElement
+                    else -> null
+                }
+
+                valueStack.push(result)
+            } else if (it != '(' && it != ' ') {
+                valueStack.push(it.digitToInt())
+            }
+        }
+
+        println("Result is ${valueStack.pop()}")
+    }
+}
+
+fun main() {
+    val alg = DijikstrasTwoStackAlgorithm("(1 + (( 2 + 3 ) * (4 * 3)))")
+    alg.execute()
 }
